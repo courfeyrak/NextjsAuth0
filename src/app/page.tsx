536e75c874921ba3e50f6a861df38c5f3d688d3f
Carlos Gutierrez <carlos.gtz.cg@gmail.com>
2025-03-21 21:55:37 -0600

    completo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,38 @@
 import { auth0 } from "@/lib/auth0";
 import './globals.css';
 
+
 export default async function Home() {
   // Fetch the user session
   const session = await auth0.getSession();
-
+  
   // If no session, show sign-up and login buttons
   if (!session) {
+
     return (
       <main>
         <a href="/auth/login?screen_hint=signup">
           <button>Sign up</button>
         </a>
+        <br/>
         <a href="/auth/login">
-          <button>Log in</button>
-        </a>
+          <button>Log in sin</button>
+        </a><br/>
+        {/* <a href="/auth/login?audience=https://51lyy4n8z0.execute-api.us-east-2.amazonaws.com/dev">
+          <button>Log in con</button>
+        </a> */}
       </main>
     );
   }
+  const token = await auth0.getAccessToken();
 
+  console.log("------TOKEN:", token);
   // If session exists, show a welcome message and logout button
   return (
     <main>
       <h1>Welcome, {session.user.name}!</h1>
       <p>
-        <a href="/auth/logout">
+        <a href="/auth/logout?returnTo=http://localhost:3000/">
           <button>Log out</button>
         </a>
       </p>
--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,3 +1,9 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
-export const auth0 = new Auth0Client();
+//export const auth0 = new Auth0Client();
+export const auth0 = new Auth0Client({
+    authorizationParameters: {
+      scope: "openid profile email",
+      audience: "https://51lyy4n8z0.execute-api.us-east-2.amazonaws.com/dev",
+    },
+  })
