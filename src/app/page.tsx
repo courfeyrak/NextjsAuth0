5f6f9cde7486dbba2842ec4b1b0928de8b80de23
Carlos Gutierrez <carlos.gtz.cg@gmail.com>
2025-04-28 17:10:41 -0600

    asdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,14 @@ export default async function Home() {
   // Fetch the user session
   const session = await auth0.getSession();
   
+  // Get the APP_BASE_URL environment variable
+  const appBaseUrl = process.env.APP_BASE_URL;
+  
   // If no session, show sign-up and login buttons
   if (!session) {
-
     return (
       <main>
+        <p>APP_BASE_URL: {appBaseUrl || 'No está definida'}</p>
         <a href="/auth/login?screen_hint=signup">
           <button>Sign up</button>
         </a>
@@ -24,12 +27,13 @@ export default async function Home() {
       </main>
     );
   }
-  const token = await auth0.getAccessToken();
+  //const token = await auth0.getAccessToken();
 
-  console.log("------TOKEN:", token);
+ //console.log("------TOKEN:", token);
   // If session exists, show a welcome message and logout button
   return (
     <main>
+      <p>APP_BASE_URL: {appBaseUrl || 'No está definida'}</p>
       <h1>Welcome, {session.user.name}!</h1>
       <p>
         <a href="/auth/logout?returnTo=http://localhost:3000/">
